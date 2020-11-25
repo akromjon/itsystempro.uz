6c1ad0ace96622f4d75d6f96ca872d817fe5c08d
Akromjon <numonov.akromjon1998@gmail.com>
2020-11-25 20:01:15 +0500

    First Step

---
+++ resources/lang/uz/lang.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'home'=>'Bosh Saxifa',
+    'about'=>'Biz Xaqimizda',
+    'product'=>'Ishlarimiz',
+    'pricing'=>'Narxlar',
+    'team'=>'Jamoa',
+    'contact'=>'Aloqa',
+    'calculator'=>'Kalkulator',
+    'start_website'=>'1 Kunda Web Saytingizga Ega Bo\'ling',
+    'website_des'=>'O\'z Biznesingiz uchun tezkor, zamonaviy saytingiz bo\'lishini hohlaysizmi?',
+    'order'=>'Buyurtma bering',
+    'see_price'=>'Narxlar bilan tanishish'
+]
+?>
